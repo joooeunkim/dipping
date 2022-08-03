8f98c0a1bd60e3f53e0d513aab2facf98bf75bd6
parksanghyeop <mon2210@naver.com>
2022-08-03 13:11:00 +0900

    Feature: [#5] 회원가입 라우터 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 import { DippinMain } from './views/dippin/DippinMain';
 import { HomeMain } from './views/home/HomeMain';
@@ -8,26 +9,30 @@ import { FindPassword } from './views/users/FindPassword';
 import { Login } from './views/users/Login';
 import { Register } from './views/users/Register';
 import { Genre } from './views/users/register_process/Genre';
+import { InterestTag } from './views/users/register_process/InterestTag';
 import { RegisterProcessLayout } from './views/users/register_process/RegisterProcessLayout';
 import { UserInfo } from './views/users/register_process/UserInfo';
 import { SetPassword } from './views/users/SetPassword';
 
 export const App = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<HomeMain />} />
-      <Route path="dippin" element={<DippinMain />} />
-      <Route path="search" element={<SearchMain />} />
-      <Route path="profile" element={<ProfileMain />} />
-      <Route path="profile/:nickname" element={<ProfileMain />} />
-    </Route>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/findPassword" element={<FindPassword />} />
-    <Route path="/setPassword" element={<SetPassword />} />
-    <Route path="/process" element={<RegisterProcessLayout />}>
-      <Route path="step1" element={<UserInfo />} />
-      <Route path="step2" element={<Genre />} />
-    </Route>
-  </Routes>
+  <ChakraProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeMain />} />
+        <Route path="dippin" element={<DippinMain />} />
+        <Route path="search" element={<SearchMain />} />
+        <Route path="profile" element={<ProfileMain />} />
+        <Route path="profile/:nickname" element={<ProfileMain />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/findPassword" element={<FindPassword />} />
+      <Route path="/setPassword" element={<SetPassword />} />
+      <Route path="/process" element={<RegisterProcessLayout />}>
+        <Route path="step1" element={<UserInfo />} />
+        <Route path="step2" element={<Genre />} />
+        <Route path="step3" element={<InterestTag />} />
+      </Route>
+    </Routes>
+  </ChakraProvider>
 );
