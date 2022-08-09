d947990593b4b594012a47d43fa5541e59c52194
Hyung Ju <ghj6282@daum.net>
2022-08-09 13:42:17 +0900

    Feature[#38] Search 초기 설정

---
+++ back/dipping/src/main/java/com/common/dipping/api/search/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.common.dipping.api.search.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/search")
+public class SearchController {
+}
