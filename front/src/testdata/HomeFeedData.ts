4ce6a0ae871ff1514c8447a010318e71fab995b5
이길상 <oat641@gmail.com>
2022-08-10 15:43:02 +0900

    Feature:[#33] 더미데이터 추가

--- front/src/testdata/HomeFeedData.ts
+++ front/src/testdata/HomeFeedData.ts
@@ -171,4 +171,136 @@ export const HomeFeedData: Array<FeedPost> = [
       },
     ],
   },
+  {
+    title: 'SimpleIsBest',
+    likes: '22',
+    article: 'no more confusion.',
+    tags: '#void',
+    last_modified: '1주',
+    user: {
+      name: 'slavv',
+      profile_image: 'https://bit.ly/3Qvuqb4',
+    },
+    playlists: [
+      {
+        title: 'LA Devotee',
+        artist: 'Panic! At The Disco',
+        albumart: 'https://i.ytimg.com/vi/RzMITAmJfeg/maxresdefault.jpg',
+        id: 'RzMITAmJfeg',
+      },
+      {
+        title: 'Stressed Out',
+        artist: '​twenty one pilots',
+        albumart: 'https://i.ytimg.com/vi/Gc4sY98Jn9I/maxresdefault.jpg',
+        id: 'Gc4sY98Jn9I',
+      },
+      {
+        title: 'Welcome To The Black Parade',
+        artist: 'My Chemical Romance',
+        albumart: 'https://i.ytimg.com/vi/G-I9csAflBs/maxresdefault.jpg',
+        id: 'G-I9csAflBs',
+      },
+      {
+        title: '백색왜성',
+        artist: '넬',
+        albumart: 'https://i.ytimg.com/vi/qRaAvEbKmt0/hqdefault.jpg',
+        id: 'qRaAvEbKmt0',
+      },
+      {
+        title: 'Dead!',
+        artist: 'My Chemical Romance',
+        albumart: 'https://i.ytimg.com/vi/H48kOqqaWv0/hqdefault.jpg',
+        id: 'H48kOqqaWv0',
+      },
+    ],
+    comments: [
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content: 'nice',
+          parent: null,
+          last_modified: '1분',
+          likes: 0,
+          userlike: false,
+        },
+      },
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content: 'I must use Lorem',
+          parent: ' ',
+          last_modified: '1시간',
+          likes: 22,
+          userlike: true,
+        },
+      },
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content: 'Good Old Days',
+          parent: null,
+          last_modified: '2시간',
+          likes: 0,
+          userlike: false,
+        },
+      },
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content:
+            'The Black Parade is the third studio album by American rock band My Chemical Romance. ' +
+            'Released in Europe on October 20, 2006, through Reprise Records, it was produced by the band with ' +
+            'Rob Cavallo, known for having produced multiple albums for the Goo Goo Dolls and Green Day. ',
+          parent: null,
+          last_modified: '2시간',
+          likes: 0,
+          userlike: false,
+        },
+      },
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content:
+            'The Black Parade is the third studio album by American rock band My Chemical Romance. ' +
+            'Released in Europe on October 20, 2006, through Reprise Records, it was produced by the band with ' +
+            'Rob Cavallo, known for having produced multiple albums for the Goo Goo Dolls and Green Day. ',
+          parent: null,
+          last_modified: '2시간',
+          likes: 0,
+          userlike: false,
+        },
+      },
+      {
+        user: {
+          name: 'mocha_oca',
+          profile_image: 'https://bit.ly/3A2BqqJ',
+        },
+        comment: {
+          content:
+            'The Black Parade is the third studio album by American rock band My Chemical Romance. ' +
+            'Released in Europe on October 20, 2006, through Reprise Records, it was produced by the band with ' +
+            'Rob Cavallo, known for having produced multiple albums for the Goo Goo Dolls and Green Day. ',
+          parent: null,
+          last_modified: '2시간',
+          likes: 0,
+          userlike: false,
+        },
+      },
+    ],
+  },
 ];
