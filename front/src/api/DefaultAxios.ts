045ec613916cb3921c78bd4ed217b0f31245a2dd
parksanghyeop <mon2210@naver.com>
2022-08-10 15:14:00 +0900

    Feature: [#45]default axiso 세팅

---
+++ front/src/api/DefaultAxios.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const SERVER_ADDRESS = 'http://i7b210.p.ssafy.io/api';
+
+const accessToken = localStorage.getItem('accessToken');
+
+export const DefaultAxios: AxiosInstance = axios.create({
+  baseURL: `${SERVER_ADDRESS}`,
+  headers: {
+    access_token: accessToken || '',
+  },
+});
+
+// 토큰 있는지 체크
+// export const checkToken = async (config: AxiosRequestConfig) => {
+//   if (!accessToken) {
+//     alert('토큰이 만료되었습니다. 다시 로그인 해주세요');
+//     // eslint-disable-next-line no-restricted-globals
+//     location.href = '/login';
+//   } else {
+//     return config;
+//   }
+// };
+
+// DefaultAxios.interceptors.request.use(checkToken);
