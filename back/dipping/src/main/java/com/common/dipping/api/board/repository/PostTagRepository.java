2668a3f1e86f39aefcba9e6e5f63e8b68823d12b
허유민 <fodleldjs2@naver.com>
2022-08-10 16:34:38 +0900

    Fix: 충돌난거 수정

--- back/dipping/src/main/java/com/common/dipping/api/board/repository/PostTagRepository.java
+++ back/dipping/src/main/java/com/common/dipping/api/board/repository/PostTagRepository.java
@@ -1,10 +1,7 @@
 package com.common.dipping.api.board.repository;
 
 import com.common.dipping.api.board.domain.entity.Board;
-<<<<<<< back/dipping/src/main/java/com/common/dipping/api/board/repository/PostTagRepository.java
-=======
 import com.common.dipping.api.board.domain.entity.Tag;
->>>>>>> back/dipping/src/main/java/com/common/dipping/api/board/repository/PostTagRepository.java
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.common.dipping.api.board.domain.entity.PostTag;
