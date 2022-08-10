fdb2d05abddbc2a696e118268ba2979601cc0be7
Hyung Ju <ghj6282@daum.net>
2022-08-10 10:29:58 +0900

    Feature[#38] 검색 DB에 저장

--- back/dipping/src/main/java/com/common/dipping/api/search/controller/SearchController.java
+++ back/dipping/src/main/java/com/common/dipping/api/search/controller/SearchController.java
@@ -4,11 +4,13 @@ package com.common.dipping.api.search.controller;
 import com.common.dipping.api.search.service.SearchService;
 import com.common.dipping.api.user.domain.dto.MiniProfileDto;
 import com.common.dipping.api.user.domain.entity.User;
+import com.common.dipping.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,10 +42,11 @@ public class SearchController {
     }
     */
     @GetMapping("/user")
-    public ResponseEntity<?> searchUser(@Param("keyword") String keyword) {
+    public ResponseEntity<?> searchUser(@AuthenticationPrincipal UserDetailsImpl userInfo, @Param("keyword") String keyword) {
         Map<String,Object> result = new HashMap<>();
         Map<String,Object> searchResult = new HashMap<>();
-        List<MiniProfileDto> userList = searchService.searchUser(keyword);
+
+        List<MiniProfileDto> userList = searchService.searchUser(keyword, userInfo);
         result.put("code", 200);
         searchResult.put("users", userList);
         result.put("data", searchResult);
--- back/dipping/src/main/java/com/common/dipping/api/search/service/SearchService.java
+++ back/dipping/src/main/java/com/common/dipping/api/search/service/SearchService.java
@@ -1,8 +1,11 @@
 package com.common.dipping.api.search.service;
 
+import com.common.dipping.api.search.domain.entity.Search;
+import com.common.dipping.api.search.repository.SearchRepository;
 import com.common.dipping.api.user.domain.dto.MiniProfileDto;
 import com.common.dipping.api.user.domain.entity.User;
 import com.common.dipping.api.user.repository.UserRepository;
+import com.common.dipping.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,8 +23,9 @@ public class SearchService {
 
     @Autowired
     private final UserRepository userRepository;
+    private final SearchRepository searchRepository;
 
-    public List<MiniProfileDto> searchUser(String keyword) {
+    public List<MiniProfileDto> searchUser(String keyword, UserDetailsImpl user) {
         List<User> usersList = userRepository.findAllByNicknameContaining(keyword);
         List<MiniProfileDto> miniProfileDtos = new ArrayList<MiniProfileDto>();
         for (int i = 0; i < usersList.size(); i++) {
@@ -31,6 +34,12 @@ public class SearchService {
             userInfo.setProfileImgUrl(usersList.get(i).getProfileImgUrl());
             miniProfileDtos.add(userInfo);
         }
+        User userId = userRepository.findById(user.getId()).orElse(null);
+        Search search = Search.builder()
+                .word(keyword)
+                .userId(userId)
+                .build();
+        searchRepository.save(search);
         return miniProfileDtos;
     }
 
