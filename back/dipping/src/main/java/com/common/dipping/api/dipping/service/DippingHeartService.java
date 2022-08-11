98e29064d9de04531729e6cdd4923c5f295615d3
허유민 <fodleldjs2@naver.com>
2022-08-11 15:31:08 +0900

    Feat: [#52] 디핑API개발을 위한 초기 셋팅

--- back/dipping/src/main/java/com/common/dipping/api/dipping/service/DippingHeartService.java
+++ back/dipping/src/main/java/com/common/dipping/api/dipping/service/DippingHeartService.java
@@ -2,17 +2,15 @@ package com.common.dipping.api.dipping.service;
 
 import com.common.dipping.api.dipping.repository.DippingHeartRepository;
 import com.common.dipping.api.dipping.repository.DippingRepository;
-import com.common.dipping.api.dipping.repository.DippingSongRepository;
 import com.common.dipping.api.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class DippingService {
+public class DippingHeartService {
 
     private final UserRepository userRepository;
     private final DippingRepository dippingRepository;
     private final DippingHeartRepository dippingHeartRepository;
-    private final DippingSongRepository dippingSongRepository;
 }
