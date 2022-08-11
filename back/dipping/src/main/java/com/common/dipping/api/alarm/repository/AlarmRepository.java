998237a57c3bb253d598e17b20750164001ee791
Hyung Ju <ghj6282@daum.net>
2022-08-11 10:42:29 +0900

    Feat[#53] 알림 기능 초기 설정

---
+++ back/dipping/src/main/java/com/common/dipping/api/alarm/repository/AlarmRepository.java
@@ -0,0 +1,7 @@
+package com.common.dipping.api.alarm.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlarmRepository {
+}
---
+++ back/dipping/src/main/java/com/common/dipping/api/alarm/service/AlarmService.java
@@ -0,0 +1,14 @@
+package com.common.dipping.api.alarm.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class AlarmService {
+}
