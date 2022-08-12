c67b1d7967c570ec369bfa7a671ed0df4cc1e123
Hyung Ju <ghj6282@daum.net>
2022-08-12 15:28:05 +0900

    Feat: [#72]디핑 검색

--- back/dipping/src/main/java/com/common/dipping/api/search/controller/SearchController.java
+++ back/dipping/src/main/java/com/common/dipping/api/search/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.common.dipping.api.search.controller;
 
 import com.common.dipping.api.board.domain.dto.BoardDto;
 import com.common.dipping.api.board.domain.entity.Board;
+import com.common.dipping.api.dipping.domain.dto.DippingResponseDto;
 import com.common.dipping.api.search.service.SearchService;
 import com.common.dipping.api.user.domain.dto.MiniProfileDto;
 import com.common.dipping.api.user.domain.entity.User;
@@ -101,5 +102,19 @@ public class SearchController {
         result.put("data", searchResult);
 
         return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/dipping")
+    public ResponseEntity<?> searchDipping(@AuthenticationPrincipal UserDetailsImpl userInfo, @Param("keyword") String keyword) {
+        Map<String,Object> result = new HashMap<>();
+        Map<String,List> searchResult = new HashMap<>();
+
+        List<DippingResponseDto> dippingList = searchService.searchDipping(keyword, userInfo);
+        result.put("code", 200);
+        searchResult.put("dippings", dippingList);
+        result.put("data", searchResult);
+
+        return ResponseEntity.ok().body(result);
+
     }
 }
