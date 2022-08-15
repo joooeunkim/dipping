c446fa8a3f5f8541e805773592e50755d4bb4a9c
Hyung Ju <ghj6282@daum.net>
2022-08-15 22:29:35 +0900

    Feat: [#49]검색 페이지 보드 추천 추가

--- back/dipping/src/main/java/com/common/dipping/api/board/domain/dto/ProfilePostDto.java
+++ back/dipping/src/main/java/com/common/dipping/api/board/domain/dto/ProfilePostDto.java
@@ -1,5 +1,6 @@
 package com.common.dipping.api.board.domain.dto;
 
+import com.common.dipping.api.board.domain.entity.Board;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,4 +9,9 @@ import lombok.Data;
 public class ProfilePostDto {
     private Long id; //포스트 하나
     private String songImgUrl; //대표 이미지
+
+    public ProfilePostDto(Board board) {
+        this.id = board.getId();
+        this.songImgUrl = board.getBoardSongs().get(0).getSongImgUrl();
+    }
 }
