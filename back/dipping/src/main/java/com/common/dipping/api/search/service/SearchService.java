a3942bebf7b82b78be4989993501dbb8764c0b93
Hyung Ju <ghj6282@daum.net>
2022-08-16 16:31:10 +0900

    Refactor: [#83]유저 아이디 추가

--- back/dipping/src/main/java/com/common/dipping/api/search/service/SearchService.java
+++ back/dipping/src/main/java/com/common/dipping/api/search/service/SearchService.java
@@ -95,7 +95,6 @@ public class SearchService {
         for (User user: users) {
             if (user.getMusicGenre() == null) {continue;}
             if (user.getId() == userInfo.getId()) {continue;}
-//            result.put(user, similarity(user.getMusicGenre(), userInfo.getMusicGenre()));
             result.put(user, recommendScore(userInfo.getMusicGenre(),user.getMusicGenre()));
         }
         List<User> keySetList = new ArrayList<>(result.keySet());
@@ -119,47 +118,6 @@ public class SearchService {
             }
         }
         return score/Math.max(user1Genre.size(), user2Genre.size());
-    }
-    private static double similarity(String s1, String s2) {
-        String longer = s1, shorter = s2;
-
-        if (s1.length() < s2.length()) {
-            longer = s2;
-            shorter = s1;
-        }
-
-        int longerLength = longer.length();
-        if (longerLength == 0) return 1.0;
-        return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
-    }
-    private static int editDistance(String s1, String s2) {
-        s1 = s1.toLowerCase();
-        s2 = s2.toLowerCase();
-        int[] costs = new int[s2.length() + 1];
-
-        for (int i = 0; i <= s1.length(); i++) {
-            int lastValue = i;
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0) {
-                    costs[j] = j;
-                } else {
-                    if (j > 0) {
-                        int newValue = costs[j - 1];
-
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
-                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-                        }
-
-                        costs[j - 1] = lastValue;
-                        lastValue = newValue;
-                    }
-                }
-            }
-
-            if (i > 0) costs[s2.length()] = lastValue;
-        }
-
-        return costs[s2.length()];
     }
 
     public List<ProfilePostDto> searchRecommendedBoards(UserDetailsImpl userDetails) {
