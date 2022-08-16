b7c65e08327520f528c7683633fe59aefd81c4df
Nelfunction <oat641@gmail.com>
2022-08-17 04:52:22 +0900

    Feature[#75] 리듀서 테스트

--- front/src/views/dippin/DippinMain.tsx
+++ front/src/views/dippin/DippinMain.tsx
@@ -8,20 +8,29 @@ import { DippinMode } from '../../components/dippin/DippinMode';
 import { DippinDetail } from '../../components/dippin/DippinDetail';
 import { authAxios } from '../../api/common';
 import { iteratorSymbol } from 'immer/dist/internal';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addPage,
+  pushDippinList,
+  setDippinList,
+  setInput,
+  setPage,
+} from '../../reducers/dippinReducer';
 
 export const DippinMain = () => {
+  const dispatch = useDispatch();
+
   // 요청에 쓰일 파라미터들
-  const [input, setInput] = useState('');
-  const [mode, setMode] = useState('recent');
-  const [page, setPage] = useState(0);
+  const input = useSelector((state: any) => state.dippinReducer.input);
+  const mode = useSelector((state: any) => state.dippinReducer.mode);
+  const page = useSelector((state: any) => state.dippinReducer.page);
 
   // 화면에 표시할 리스트
-  const [dippinlist, setDippinList] = useState<Array<FeedPost>>([]);
-  const postfeeds = HomeFeedData; // 더미 데이터
+  const dippinlist: FeedPost[] = useSelector((state: any) => state.dippinReducer.dippinlist);
 
   // 검색 창 입력
   const onKeyInput = (e: any) => {
-    if (e.key === 'Enter') setInput(e.target.value);
+    if (e.key === 'Enter') dispatch(setInput(e.target.value));
   };
 
   // 검색어나 모드가 바뀌면 페이지 초기화
@@ -29,13 +38,9 @@ export const DippinMain = () => {
     console.log('DippinMain: input or mode changed');
     window.scrollTo(0, 0);
     endRef.current = false;
-    setPage(data => 0);
-    setDippinList([]);
+    dispatch(setPage(0));
     getDippinPage(input, mode, 0);
-    //setPage(data => data + 1);
-    // const observer = new IntersectionObserver(obs, { threshold: 0.5 });
-    // if (obsRef.current) observer.observe(obsRef.current);
-  }, [input, mode]);
+  }, [dispatch, input, mode]);
 
   // 새 요청 받아서 리스트에 추가
   useEffect(() => {
@@ -52,14 +57,16 @@ export const DippinMain = () => {
     const res: any = await authAxios.get('/dipping', {
       params: {
         sort: mode,
-        pageNum: page,
+        pageNum: page + 1,
       },
     });
+    console.log(res);
 
     if (res.data) {
       if (res.data.code === 201) {
         console.log('없음');
         endRef.current = true; //마지막 페이지일 경우
+        if (page === 0) dispatch(setDippinList([]));
       } else {
         const posts = res.data.data.posts;
         const list: FeedPost[] = posts.map((e: any) => {
@@ -85,7 +92,8 @@ export const DippinMain = () => {
           };
         });
 
-        setDippinList(data => [...data, ...list]);
+        if (page === 0) dispatch(setDippinList(list));
+        else dispatch(pushDippinList(list));
       }
       preventRef.current = true;
     }
@@ -114,7 +122,7 @@ export const DippinMain = () => {
       preventRef.current = false; //옵저버 중복 실행 방지
       // 실행하고 싶은 것
       console.log('DippinMain: call setPage');
-      setPage(data => data + 1);
+      dispatch(addPage());
     }
   };
   // 무한 스크롤로 업데이트 하기 위한 코드 end
@@ -138,7 +146,7 @@ export const DippinMain = () => {
         dippinid={dippinid}
         setDippinId={setDippinId}
       />
-      <DippinMode mode={mode} setMode={setMode} />
+      <DippinMode />
       {dippinlist.length > 0 ? (
         dippinlist.map((item, index) => (
           <div
