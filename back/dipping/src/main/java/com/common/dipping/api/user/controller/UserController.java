714b3902f794ad447269211d0d8c6ac06688c977
kimjooeun <ducky_313@naver.com>
2022-08-18 10:23:55 +0900

    Feature[#94] : 서버용 프로필 이미지 저장 경로 변경

--- back/dipping/src/main/java/com/common/dipping/api/user/controller/UserController.java
+++ back/dipping/src/main/java/com/common/dipping/api/user/controller/UserController.java
@@ -222,7 +222,8 @@ public class UserController {
         if (file!=null) {
             // 파일을 저장할 폴더 지정
             Resource res = resourceLoader.getResource("classpath:static/upload");
-            String canonicalPath = res.getFile().getCanonicalPath();
+            //String canonicalPath = res.getFile().getCanonicalPath();
+            String canonicalPath = "home"+File.separator+"ubuntu"+File.separator+"S07P12B210"+File.separator+"back"+File.separator+"dipping"+File.separator+"build"+File.separator+"resources"+File.separator+"main"+File.separator+"static"+File.separator+"upload";
             System.out.println("file upload canonical path : "+ canonicalPath);
             File folder = new File(canonicalPath);
             if (!folder.exists()){
