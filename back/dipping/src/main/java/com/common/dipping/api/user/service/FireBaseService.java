7c73730a4c2ead643ec5fe8ab42a5eb4a8145c28
kimjooeun <ducky_313@naver.com>
2022-08-18 17:53:09 +0900

    Feature[#94] : 프로필 이미지 파이어베이스 저장

--- back/dipping/src/main/java/com/common/dipping/api/user/service/FireBaseService.java
+++ back/dipping/src/main/java/com/common/dipping/api/user/service/FireBaseService.java
@@ -2,6 +2,7 @@ package com.common.dipping.api.user.service;
 
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class FireBaseService {
     @Value("${app.firebase-bucket}")
     private String firebaseBucket;
 
-    public String uploadFiles(MultipartFile file, String nameFile) throws IOException {
+    public String uploadFiles(MultipartFile file, String nameFile) throws IOException{
         Bucket bucket = StorageClient.getInstance().bucket(firebaseBucket);
         InputStream content = new ByteArrayInputStream(file.getBytes());
         Blob blob = bucket.create(nameFile.toString(), content, file.getContentType());
